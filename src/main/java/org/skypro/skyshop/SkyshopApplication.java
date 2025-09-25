b2daf54c6879291108179b646fdea4000a3dbf6f
EvgenyAlexand <bragin-e@mail.ru>
2025-09-25 13:33:18 +0700

    Что-то делаем, что не понятно ...

--- src/main/java/org/skypro/skyshop/SkyshopApplication.java
+++ src/main/java/org/skypro/skyshop/SkyshopApplication.java
@@ -3,7 +3,7 @@ package org.skypro.skyshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication		// Аннотация для запуска Spring Boot
 public class SkyshopApplication {
 
 	public static void main(String[] args) {
--- src/main/java/org/skypro/skyshop/controller/ShopController.java
+++ src/main/java/org/skypro/skyshop/controller/ShopController.java
@@ -3,22 +3,45 @@ package org.skypro.skyshop.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController             // аннотация Spring для создания REST API
 public class ShopController {
-    private int count;
+//    private int count;
+//
+//    // http://localhost:8080
+//    @GetMapping             // GET запрос
+//    public String hello() {
+//        return "Hello, world!";
+//    }
+//
+//    // http://localhost:8080/counter
+//    @GetMapping("/counter")
+//    public String count() {
+//        this.count++;
+//        return "Количество запросов: " + this.count;
+//    }
 
-    // http://localhost:8080
-    @GetMapping
-    public String hello() {
-        return "Hello, world!";
-    }
 
-    // http://localhost:8080/counter
-    @GetMapping("/counter")
-    public String count() {
-        this.count++;
-        return "Количество запросов: " + this.count;
-    }
+//    private final StorageService storageService;
+//    private final SearchService searchService;
+//    public ShopController(StorageService storageService, SearchService searchService) {
+//        this.storageService = storageService;
+//        this.searchService = searchService;
+//    }
+//
+//    @GetMapping("/products")
+//    public Collection<Product> getAllProduct() {
+//        return storageService.getAllProduct();
+//    }
+//
+//    @GetMapping("/articles")
+//    public Collection<Article> getAllArticles() {
+//        return storageService.getAllArticle();
+//    }
+//
+//    @GetMapping("/search")
+//    public Collection<SearchResult> search(@RequestParam("pattern") String pattern) {
+//        return searchService.search(pattern);
+//    }
 
 
 }
